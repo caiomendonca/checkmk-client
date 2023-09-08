f534dac8eb6e96dc4177150dbc42a67931ce5e16
Caio Mendonça <caiojose3@hotmail.com>
2023-09-08 11:49:27 -0300

    Update ubuntu_install_nofw.sh

--- linux/ubuntu_install_nofw.sh
+++ linux/ubuntu_install_nofw.sh
@@ -44,6 +44,7 @@ CMKIP=$(nslookup cmk-server | grep -A1 Name | tail -n1 | sed 's/Address: //')
 ## OBS: linha acima com nslookup precisa alterar para variavel e testar
 # instala pacotes necessarios basicos
 apt update -y
+apt install nslookup -y
 apt install curl -y
 apt install wget -y
 #apt install iptables-persistent -y
